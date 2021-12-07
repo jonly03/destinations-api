6bed75eab6bafe7a29323b9b9775d50f51d39007
Nelly Sugu <jonly03@gmail.com>
2021-12-07 12:03:19 -0500

    fix fetch bug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fetch = require("node-fetch");
 const cors = require("cors");
 let { destinations } = require("./db");
 const { generateUniqueId, getUnsplashPhoto } = require("./services");
--- services.js
+++ services.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 function generateUniqueId() {
   // "123456"
   let id = "";
