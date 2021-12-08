2d66d547b7950a87d33be7269afd8537327a1a2a
Nelly Sugu <jonly03@gmail.com>
2021-12-08 15:17:58 -0500

    hidden secret keys

--- index.js
+++ index.js
@@ -1,114 +1,120 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-let { destinations } = require("./db");
-const { generateUniqueId, getUnsplashPhoto } = require("./services");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const { getUnsplashPhoto } = require("./services");
 
 const server = express();
 server.use(express.json());
 server.use(cors());
-server.use(express.urlencoded());
+server.use(express.urlencoded({ extended: true }));
 
-// const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT
+const MongoDB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.whkkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-let PORT = process.env.PORT || 3000;
+const client = new MongoClient(MongoDB_URL);
 
-// if (process.env.PORT !== undefined){
-//   PORT = process.env.PORT
-// }
+client.connect().then(() => {
+  const db = client.db("first_mongodb");
 
-server.listen(PORT, function () {
-  console.log(`Server listening on PORT ${PORT}`);
-});
+  const destinations = db.collection("destinations");
 
-// POST => create destinations
-// data => {name^, location^, photo, description}
-server.post("/destinations", async (req, res) => {
-  const { name, location, description } = req.body;
+  // const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT
 
-  // Make sure we have a name AND location
-  if (
-    name === undefined ||
-    name.length === 0 ||
-    location === undefined ||
-    location.length === 0
-  ) {
-    return res
-      .status(400)
-      .json({ message: "Name and Location are both required" });
-  }
+  let PORT = process.env.PORT || 3000;
 
-  const dest = { id: generateUniqueId(), name, location };
+  // if (process.env.PORT !== undefined){
+  //   PORT = process.env.PORT
+  // }
 
-  dest.photo = await getUnsplashPhoto({ name, location });
+  server.listen(PORT, function () {
+    console.log(`Server listening on PORT ${PORT}`);
+  });
 
-  if (description && description.length !== 0) {
-    dest.description = description;
-  }
+  // POST => create destinations
+  // data => {name^, location^, photo, description}
+  server.post("/destinations", async (req, res) => {
+    const { name, location, description } = req.body;
 
-  destinations.push(dest);
-
-  res.redirect("/destinations");
-});
-
-// GET => read destinations
-// accepts the follow query parameters
-// continent
-server.get("/destinations", (req, res) => {
-  res.send(destinations);
-});
-
-// PUT => edit a destination
-server.put("/destinations/", async (req, res) => {
-  const { id, name, location, description } = req.body;
-
-  if (id === undefined) {
-    return res.status(400).json({ message: "id is required" });
-  }
-
-  if (name !== undefined && name.length === 0) {
-    return res.status(400).json({ message: "Name can't be empty" });
-  }
-
-  if (location !== undefined && location.length === 0) {
-    return res.status(400).json({ message: "Location can't be empty" });
-  }
-
-  for (const dest of destinations) {
-    if (dest.id === id) {
-      if (name !== undefined) {
-        dest.name = name;
-      }
-
-      if (location !== undefined) {
-        dest.location = location;
-      }
-
-      if (name !== undefined || location !== undefined) {
-        dest.photo = await getUnsplashPhoto({
-          name: dest.name,
-          location: dest.location,
-        });
-      }
-
-      if (description !== undefined) {
-        dest.description = description;
-      }
-
-      return res.json(dest);
+    // Make sure we have a name AND location
+    if (
+      name === undefined ||
+      name.length === 0 ||
+      location === undefined ||
+      location.length === 0
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Name and Location are both required" });
     }
-  }
-});
 
-// DELETE => delete a destination
-// HOW TO GET THE ID from the reqs
-// route parameters /destinations/:id => req.params.id
-// query /destinations?id=198745 => req.query.id
-server.delete("/destinations/:id", (req, res) => {
-  const destId = req.params.id;
+    const dest = { name, location };
 
-  const newDestinations = destinations.filter((dest) => dest.id !== destId);
+    dest.photo = await getUnsplashPhoto({ name, location });
 
-  destinations = newDestinations;
+    if (description && description.length !== 0) {
+      dest.description = description;
+    }
 
-  res.redirect(303, "/destinations");
+    destinations.insertOne(dest);
+
+    res.redirect("/destinations");
+  });
+
+  // GET => read destinations
+  // accepts the follow query parameters
+  // continent
+  server.get("/destinations", async (req, res) => {
+    const data = await destinations.find({}).toArray();
+    res.send(data);
+  });
+
+  // PUT => edit a destination
+  server.put("/destinations/", async (req, res) => {
+    const { id, name, location, description } = req.body;
+
+    if (id === undefined) {
+      return res.status(400).json({ message: "id is required" });
+    }
+
+    if (name !== undefined && name.length === 0) {
+      return res.status(400).json({ message: "Name can't be empty" });
+    }
+
+    if (location !== undefined && location.length === 0) {
+      return res.status(400).json({ message: "Location can't be empty" });
+    }
+
+    const newDest = {
+      name: name,
+      location: location,
+      photo: await getUnsplashPhoto({ name, location }),
+    };
+
+    if (description !== undefined) {
+      newDest.description = description;
+    }
+
+    const updateDest = await destinations.findOneAndUpdate(
+      { _id: ObjectId(id) },
+      { $set: newDest }
+    );
+
+    return res.json(updateDest.value);
+  });
+
+  // DELETE => delete a destination
+  // HOW TO GET THE ID from the reqs
+  // route parameters /destinations/:id => req.params.id
+  // query /destinations?id=198745 => req.query.id
+  server.delete("/destinations/:id", (req, res) => {
+    const destId = req.params.id;
+
+    const newDestinations = destinations.filter((dest) => dest.id !== destId);
+
+    destinations = newDestinations;
+
+    res.redirect(303, "/destinations");
+  });
 });
