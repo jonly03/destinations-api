6ddb81281c09a811f68bca74bafb9ef4f7381c36
Nelly Sugu <jonly03@gmail.com>
2021-12-08 15:28:30 -0500

    old age but with env keys

--- services.js
+++ services.js
@@ -13,7 +13,7 @@ function generateUniqueId() {
 }
 
 async function getUnsplashPhoto({ name, location }) {
-  const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=ZUAPn3ODLz7xSJEa_kQZY4B2kfKSccXz6f3jBytjcGE&query=${name} ${location}`;
+  const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_KEY}&query=${name} ${location}`;
 
   const fetchRes = await fetch(UNSPLASH_URL);
   const data = await fetchRes.json();
